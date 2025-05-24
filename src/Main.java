3f79ab50b95ef18c9e7425f6d27c8fb2c333c2ee
hackercrcracker5 <hackercracker@mail.bg>
2025-05-24 20:51:42 +0300

    Realiziran konstruktor na Stock i realiziran klas s konstruktor na Employee # Conflicts: #	.gitignore

--- src/Main.java
+++ src/Main.java
@@ -1,26 +1,28 @@
+import java.sql.ClientInfoStatus;
 import java.time.LocalDate;
 import java.time.temporal.ChronoUnit;
+import java.util.List;
+
 class Stock
 {
     private String id="";
     private String name;
-    private double price;
+    private double localPrice;
     private boolean  isFood;
     private LocalDate expirationDate;
-
+    private int quantitiy;
 
     public String getId() {
         return id;
     }
-
     public void setId(String id) {
         this.id = id;
     }
 
+
     public String getName() {
         return name;
     }
-
     public void setName(String name) {
         this.name = name;
     }
@@ -29,19 +31,19 @@ class Stock
     public boolean getIsFood() {
             return isFood;
     }
-
     public void setIsFood(boolean isFood) {
         this.isFood = isFood;
     }
 
 
-    public double getPrice() {
-        return price;
+    public double getLocalPrice() {
+        return localPrice;
     }
     public void setPrice(double price)
     {
-        this.price = price;
+        this.localPrice = price;
     }
+
 
     public LocalDate getExpirationDate() {
         return expirationDate;
@@ -50,6 +52,66 @@ class Stock
         this.expirationDate = expirationDate;
     }
 
+
+    public int getQuantitiy() {
+        return quantitiy;
+    }
+    public void setQuantitiy(int quantitiy)
+    {
+        this.quantitiy = quantitiy;
+    }
+
+
+    Stock(String id, String name, double price, boolean isFood, LocalDate expirationDate, int quantitiy)
+    {
+        setId(id);
+        setName(name);
+        setIsFood(isFood);
+        setExpirationDate(expirationDate);
+        setQuantitiy(quantitiy);
+    }
+}
+
+
+class Employee{
+    private String name;
+    private String id;
+    private double salary;
+
+
+    public String getName()
+    {
+        return name;
+    }
+    public void setName(String name)
+    {
+     this.name = name;
+    }
+
+
+    public String getId()
+    {
+        return id;
+    }
+    public void setId(String id) {
+        this.id = id;
+    }
+
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+    public double getSalary() {
+        return salary;
+    }
+
+
+    public Employee(String id, String name, double salary)
+    {
+        setId(id);
+        setName(name);
+        setSalary(salary);
+    }
 }
 
 public class Main {
